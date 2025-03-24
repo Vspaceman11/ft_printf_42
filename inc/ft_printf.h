bb8f77afa2eae1a79c6897b8ca0afde51dc139c3
Vspaceman11 <pushkarov.vladyslav@gmail.com>
2025-03-24 18:06:09 +0100

    Created Makefile, ft_printf function and header for it

---
+++ inc/ft_printf.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vpushkar <vpushkar@student.42heilbronn.de> +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/24 16:21:43 by vpushkar          #+#    #+#             */
+/*   Updated: 2025/03/24 17:00:36 by vpushkar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+#include <stdarg.h>
+#include "libft.h"
+
+int	ft_printf(const char *, ...);
+
+#endif
