8742873cddc30a077a4c008c387b26c21aec7265
Vspaceman11 <pushkarov.vladyslav@gmail.com>
2025-03-26 17:35:53 +0100

    Complete functions, everything is working. Need to fit norm

--- src/ft_printf.c
+++ src/ft_printf.c
@@ -6,15 +6,19 @@
 /*   By: vpushkar <vpushkar@student.42heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/24 16:17:09 by vpushkar          #+#    #+#             */
-/*   Updated: 2025/03/25 17:03:38 by vpushkar         ###   ########.fr       */
+/*   Updated: 2025/03/26 17:33:19 by vpushkar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/ft_printf.h"
 
-static int	len_count(long n);
-static void	ft_uns_putnbr_fd(unsigned int n, int fd);
+static int	ft_uns_putnbr_fd(unsigned int n, int fd);
 static int	ft_putptr_fd(uintptr_t ptr, int fd);
+static int	ft_puthex_low_fd(unsigned int n, int fd);
+static int	ft_puthex_upp_fd(unsigned int n, int fd);
+static int	ft_putchar_cust_fd(char c, int fd);
+static int	ft_putstr_cust_fd(char *s, int fd);
+static int	ft_putnbr_cust_fd(int n, int fd);
 
 int	ft_printf(const char *format, ...)
 {
@@ -36,66 +40,76 @@ int	ft_printf(const char *format, ...)
 			if (format[i + 1] == 'i' || format[i + 1] == 'd')
 			{
 				digit = va_arg(args, int);
-				ft_putnbr_fd(digit, 1);
-				count += len_count(digit);
+				count += ft_putnbr_cust_fd(digit, 1);
+				if (count == -1)
+					return (-1);
 				i += 2;
-				continue;
+				continue ;
 			}
 			else if (format[i + 1] == 'c')
 			{
-				ft_putchar_fd((char)va_arg(args, int), 1);
+				if (ft_putchar_cust_fd((char)va_arg(args, int), 1) == -1)
+					return (-1);
 				i += 2;
 				count++;
-				continue;
+				continue ;
 			}
 			else if (format[i + 1] == 's')
 			{
 				str = va_arg(args, char *);
-				if (str)
-				{
-					ft_putstr_fd(str, 1);
-					count += ft_strlen(str);
-				}
-				else
-				{
-					ft_putstr_fd("(null)", 1);
-					count += 6;
-				}
+				count += ft_putstr_cust_fd(str, 1);
+				if (count == -1)
+					return (-1);
 				i += 2;
-				continue;
+				continue ;
 			}
 			else if (format[i + 1] == 'u')
 			{
 				digit = va_arg(args, unsigned int);
-				ft_uns_putnbr_fd(digit, 1);
-				count += len_count(digit);
+				count += ft_uns_putnbr_fd(digit, 1);
+				if (count == -1)
+					return (-1);
 				i += 2;
-				continue;
+				continue ;
 			}
 			else if (format[i + 1] == 'p')
 			{
 				ptr = va_arg(args, void *);
 				count += ft_putptr_fd((uintptr_t)ptr, 1);
-				i +=2;
-				continue;
+				if (count == -1)
+					return (-1);
+				i += 2;
+				continue ;
 			}
-			// else if (format[i + 1] == 'x')
-			// {
-
-			// }
-			// else if (format[i + 1] == 'X')
-			// {
-
-			// }
+			else if (format[i + 1] == 'x')
+			{
+				digit = va_arg(args, unsigned int);
+				count += ft_puthex_low_fd(digit, 1);
+				if (count == -1)
+					return (-1);
+				i += 2;
+				continue ;
+			}
+			else if (format[i + 1] == 'X')
+			{
+				digit = va_arg(args, unsigned int);
+				count += ft_puthex_upp_fd(digit, 1);
+				if (count == -1)
+					return (-1);
+				i += 2;
+				continue ;
+			}
 			else if (format[i + 1] == '%')
 			{
-				ft_putchar_fd('%', 1);
-				count++;
+				count += ft_putchar_cust_fd('%', 1);
+				if (count == -1)
+					return (-1);
 				i += 2;
-				continue;
+				continue ;
 			}
 		}
-		ft_putchar_fd(format[i], 1);
+		if (ft_putchar_cust_fd(format[i], 1) == -1)
+			return (-1);
 		i++;
 		count++;
 	}
@@ -103,45 +117,39 @@ int	ft_printf(const char *format, ...)
 	return (count);
 }
 
-static int	len_count(long n)
+static int	ft_uns_putnbr_fd(unsigned int n, int fd)
 {
-	int	len;
+	int	count;
 
-	len = 0;
-	if (n == 0)
-		return (1);
-	if (n < 0)
-		len++;
-	while (n != 0)
-	{
-		n = n / 10;
-		len++;
-	}
-	return (len);
-}
-static void	ft_uns_putnbr_fd(unsigned int n, int fd)
-{
+	count = 0;
 	if (n >= 10)
 	{
-		ft_uns_putnbr_fd(n / 10, fd);
+		count += ft_uns_putnbr_fd(n / 10, fd);
+		if (count == -1)
+			return (-1);
 	}
-	ft_putchar_fd(n % 10 + '0', fd);
+	if (ft_putchar_cust_fd(n % 10 + '0', fd) == -1)
+		return (-1);
+	count++;
+	return (count);
 }
+
 static int	ft_putptr_fd(uintptr_t ptr, int fd)
 {
 	char	*hex_digits;
 	char	hex[16];
 	int		i;
-	int	count;
+	int		count;
 
 	hex_digits = "0123456789abcdef";
 	i = 0;
 	count = 2;
-	ft_putstr_fd("0x", fd);
+	if (ft_putstr_cust_fd("0x", fd) == -1)
+		return (-1);
 	if (ptr == 0)
 	{
-		ft_putchar_fd('0', fd);
-		return (count + 1);
+		ft_putchar_cust_fd('0', fd);
+		return (3);
 	}
 	while (ptr != 0)
 	{
@@ -150,6 +158,89 @@ static int	ft_putptr_fd(uintptr_t ptr, int fd)
 	}
 	count = i;
 	while (--i >= 0)
-		ft_putchar_fd(hex[i], fd);
+		ft_putchar_cust_fd(hex[i], fd);
 	return (count + 2);
+}
+
+static int	ft_puthex_low_fd(unsigned int n, int fd)
+{
+	char	*hex_digits;
+	int		count;
+
+	hex_digits = "0123456789abcdef";
+	count = 0;
+	if (n >= 16)
+		count += ft_puthex_low_fd(n / 16, fd);
+	if (count == -1)
+		return (-1);
+	if (write(fd, &hex_digits[n % 16], 1) == -1)
+		return (-1);
+	count++;
+	return (count);
+}
+
+static int	ft_puthex_upp_fd(unsigned int n, int fd)
+{
+	char	*hex_digits;
+	int		count;
+
+	hex_digits = "0123456789ABCDEF";
+	count = 0;
+	if (n >= 16)
+		count += ft_puthex_upp_fd(n / 16, fd);
+	if (count == -1)
+		return (-1);
+	if (write(fd, &hex_digits[n % 16], 1) == -1)
+		return (-1);
+	count++;
+	return (count);
+}
+
+static int	ft_putchar_cust_fd(char c, int fd)
+{
+	if (write(fd, &c, 1) == -1)
+		return (-1);
+	return (1);
+}
+
+static int	ft_putstr_cust_fd(char *s, int fd)
+{
+	int	i;
+
+	if (!s)
+		s = "(null)";
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (ft_putchar_cust_fd(s[i], fd) == -1)
+			return (-1);
+		i++;
+	}
+	return (i);
+}
+
+static int	ft_putnbr_cust_fd(int n, int fd)
+{
+	int	count;
+
+	count = 0;
+	if (n == -2147483648)
+		return (ft_putstr_cust_fd("-2147483648", fd));
+	if (n < 0)
+	{
+		if (ft_putchar_cust_fd('-', fd) == -1)
+			return (-1);
+		n = -n;
+		count ++;
+	}
+	if (n >= 10)
+	{
+		count += ft_putnbr_cust_fd(n / 10, fd);
+		if (count == -1)
+			return (-1);
+	}
+	if (ft_putchar_cust_fd(n % 10 + '0', fd) == -1)
+		return (-1);
+	count++;
+	return (count);
 }
